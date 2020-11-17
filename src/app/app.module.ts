684f810f3b3fc2a2b0fdcfb760e529611d694984
Wassim-Gz <wassim.guezguez@esprit.tn>
2020-11-17 22:41:33 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import 'hammerjs';
+
+import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,12 +1,12 @@
-   export class Dish{
-       id : string;
-       name: string;
-       image: string;
-       category: string;
-       featured: boolean;
-       label: string;
-       price: string;
-       description: string;
+   export class Dish {
+       id!: string;
+       name!: string;
+       image!: string;
+       category!: string;
+       featured!: boolean;
+       label!: string;
+       price!: string;
+       description!: string;
    }
 
    
